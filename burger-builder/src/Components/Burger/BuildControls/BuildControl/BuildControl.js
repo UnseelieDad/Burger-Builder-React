bc6685f111b82ed0dee5b9d7adef3f35bb942ec2
Seth Martin <voiceofsethmartin@gmail.com>
2020-05-28 18:52:23 -0500

    Fixed build control styling.

--- burger-builder/src/Components/Burger/BuildControls/BuildControl/BuildControl.js
+++ burger-builder/src/Components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -1,11 +1,16 @@
-import React from "react";
-import classes from "./BuildControl.module.css";
-// Each control element has a label, an add, and remove button.
+import React from 'react';
+
+import classes from './BuildControl.module.css';
+
 const buildControl = (props) => (
-    <div classNae={classes.BuildControl}>
-        <div className={classes.Label} >{props.label}</div>
-        <button className={classes.Less} >Less</button>
-        <button className={classes.More} >More</button>
+    <div className={classes.BuildControl}>
+        <div className={classes.Label}>{props.label}</div>
+        <button 
+            className={classes.Less} 
+        >Less</button>
+        <button 
+            className={classes.More} 
+        >More</button>
     </div>
 );
 
