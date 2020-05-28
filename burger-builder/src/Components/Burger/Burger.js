cf5c17311cd5cd9f664c6ec3f563f2f6bc4d21b1
Seth Martin <voiceofsethmartin@gmail.com>
2020-05-28 17:12:53 -0500

    Dynamically creating ingredients

--- burger-builder/src/Components/Burger/Burger.js
+++ burger-builder/src/Components/Burger/Burger.js
@@ -3,11 +3,21 @@ import classes from "./Burger.module.css"
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = (props) => {
+    // Props.ingredients initially comes in as an object.
+    // Use Object.keys to create an array of property names, but not the values
+    const transformedIngredients = Object.keys(props.ingredients)
+        // For each key in ingredients
+        .map(igKey => {
+            // Create an array equal to the size of the key's value
+            // Then for each element in that array, create a burger ingredient
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurgerIngredient key={igKey + i} type={igKey}/>
+            });
+        });
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom" />
         </div>
     );
