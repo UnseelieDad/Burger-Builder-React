c6caaee1a9de214bece5465c58510d8a0b9babcd
Seth Martin <voiceofsethmartin@gmail.com>
2020-06-01 00:29:48 -0500

    Moving layout to containers since it's a class and has state

--- burger-builder/src/Containers/Layout/Layout.js
+++ burger-builder/src/Containers/Layout/Layout.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import Aux from "../../HOC/Auxilary";
 import classes from "./Layout.module.css";
-import Toolbar from "../Navigation/Toolbar/Toolbar";
-import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
+import Toolbar from "../../Components/Navigation/Toolbar/Toolbar";
+import SideDrawer from "../../Components/Navigation/SideDrawer/SideDrawer";
 
 // High Level component to contain the layout of the app
 class Layout extends Component {
