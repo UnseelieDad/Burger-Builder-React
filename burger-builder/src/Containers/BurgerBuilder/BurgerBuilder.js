d0e36f1cded2b2b7807a62a79e554fa34921aeb2
Seth Martin <voiceofsethmartin@gmail.com>
2020-06-03 17:38:22 -0500

    Added global error handling for requests

--- burger-builder/src/Containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/Containers/BurgerBuilder/BurgerBuilder.js
@@ -6,6 +6,7 @@ import Modal from "../../Components/UI/Modal/Modal";
 import OrderSummary from "../../Components/Burger/OrderSummary/OrderSummary";
 import axios from "../../axios-orders";
 import Spinner from "../../Components/UI/Spinner/Spinner";
+import withErrorHandler from "../../HOC/withErrorHandler/withErrorHandler";
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -164,4 +165,4 @@ class BurgerBuilder extends Component {
   }
 }
 
-export default BurgerBuilder;
+export default withErrorHandler(BurgerBuilder, axios);
