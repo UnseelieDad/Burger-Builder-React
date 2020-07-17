cb96106278dcb58941556153296b681a757c968e
Seth Martin <voiceofsethmartin@gmail.com>
2020-07-17 14:46:10 -0500

    Created order compoent

--- burger-builder/src/App.js
+++ burger-builder/src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
+import { Route } from 'react-router-dom';
+
 import Layout from "./Containers/Layout/Layout";
 import BurgerBuilder from "./Containers/BurgerBuilder/BurgerBuilder";
 import Checkout from './Containers/Checkout/Checkout';
-import { Route } from 'react-router-dom';
+import Orders from './Containers/Orders/Orders';
 
 class App extends Component {
   render() {
@@ -11,6 +13,7 @@ class App extends Component {
         <Layout>
           <Route path="/" exact component={BurgerBuilder} />
           <Route path="/checkout" component={Checkout} />
+          <Route path="/orders" component={Orders} />
         </Layout>
       </div>
     );
