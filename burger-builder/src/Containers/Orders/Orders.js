c8a51cd2ca640483dc25e822160502787cfb2437
Seth Martin <voiceofsethmartin@gmail.com>
2020-07-20 11:50:46 -0500

    fixing orders

--- burger-builder/src/Containers/Orders/Orders.js
+++ burger-builder/src/Containers/Orders/Orders.js
@@ -1,48 +1,50 @@
 import React, { Component } from "react";
-import axios from '../../axios-orders';
+import axios from "../../axios-orders";
 
-import Order from '../../Components/Order/Order';
-import withErrorHandler from '../../HOC/withErrorHandler/withErrorHandler';
+import Order from "../../Components/Order/Order";
+import withErrorHandler from "../../HOC/withErrorHandler/withErrorHandler";
 
 class Orders extends Component {
+  state = {
+    orders: [],
+    loading: true,
+  };
 
-    state = {
-        orders: [],
-        loading: true
-    }
+  componentDidMount() {
+    // Retrieve orders from the database
+    axios
+      .get("/orders.json")
+      .then((res) => {
+        const fetchedOrders = [];
+        // push orders onto the fetchedOrders array
+        for (let key in res.data) {
+          fetchedOrders.push({
+            id: key,
+            ...res.data[key],
+          });
+        }
+        this.setState({ loading: false, orders: fetchedOrders });
+      })
+      .catch((err) => {
+        this.setState({ loading: false });
+      });
+  }
 
-    componentDidMount() {
-        // Retrieve orders from the database
-        axios.get('/orders.json')
-            .then(res => {
-                const fetchedOrders = [];
-                // push orders onto the fetchedOrders array
-                for (let key in res.data) {
-                    fetchedOrders.push({
-                        id: key,
-                        ...res.data[key]
-                    });
-                }
-                this.setState({loading: false, orders: fetchedOrders});
-            })
-            .catch(err => {
-                this.setState({loading: false});
-            });
-    }
-
-    render() {
-        return (
-            <div>
-                {this.state.orders.map(order => {
-                    <Order 
-                        ingredients={order.ingredients}
-                        key={order.id}
-                        price={order.price} 
-                    />
-                })}
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        {this.state.orders.map((order) => {
+          return (
+            <Order
+              ingredients={order.ingredients}
+              key={order.id}
+              price={order.price}
+            />
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default withErrorHandler(Orders, axios);
