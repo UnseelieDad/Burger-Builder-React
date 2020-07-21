9ec83c56a22ca177fa0c1f89760002a20e584118
Seth Martin <voiceofsethmartin@gmail.com>
2020-07-21 15:21:37 -0500

    Adding validation feedback

--- burger-builder/src/Components/UI/Input/Input.js
+++ burger-builder/src/Components/UI/Input/Input.js
@@ -4,6 +4,11 @@ import classes from "./Input.module.css";
 
 const input = (props) => {
   let inputElement = null;
+  const inputClasses = [classes.InputElement];
+
+  if (props.invalid && props.shouldValidate) {
+    inputClasses.push(classes.Invalid);
+  }
 
   // determine what kind of input to use based off
   // of the passed in props
@@ -13,7 +18,7 @@ const input = (props) => {
       inputElement = (
         <input
           onChange={props.changed}
-          className={classes.InputElement}
+          className={inputClasses.join(' ')}
           {...props.elementConfig}
           value={props.value}
         />
@@ -23,7 +28,7 @@ const input = (props) => {
       inputElement = (
         <textarea
           onChange={props.changed}
-          className={classes.InputElement}
+          className={inputClasses.join(' ')}
           {...props.elementConfig}
           value={props.value}
         />
@@ -33,7 +38,7 @@ const input = (props) => {
       inputElement = (
         <select
           onChange={props.changed}
-          className={classes.InputElement}
+          className={inputClasses.join(' ')}
           value={props.value}
         >
           {
