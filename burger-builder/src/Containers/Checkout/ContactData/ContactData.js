99bbf7fa0d779fa898c4ed8e8c4baf19d473c470
Seth Martin <voiceofsethmartin@gmail.com>
2020-07-26 15:29:35 -0500

    Setting up redux for orders

--- burger-builder/src/Containers/Checkout/ContactData/ContactData.js
+++ burger-builder/src/Containers/Checkout/ContactData/ContactData.js
@@ -108,18 +108,6 @@ class ContactData extends Component {
       price: this.props.price,
       orderData: formData
     };
-    // Send data to a firebase endpoint
-    axios
-      .post("/orders.json", order)
-      .then((response) => {
-        console.log(response);
-        this.setState({ loading: false });
-        this.props.history.push("/");
-      })
-      .catch((error) => {
-        console.log(error);
-        this.setState({ loading: false });
-      });
   };
 
   // validate form fields
