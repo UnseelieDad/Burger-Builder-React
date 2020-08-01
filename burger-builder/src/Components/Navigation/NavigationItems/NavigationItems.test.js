1446c1863aff3ce5250517d48125a3ad21b5c3b2
Seth Martin <voiceofsethmartin@gmail.com>
2020-07-31 19:05:59 -0500

    Added a second test with cleaner helper methods

--- burger-builder/src/Components/Navigation/NavigationItems/NavigationItems.test.js
+++ burger-builder/src/Components/Navigation/NavigationItems/NavigationItems.test.js
@@ -7,9 +7,18 @@ import NavigationItem from "./NavigationItem/NavigationItem";
 configure({ adapter: new Adapter() });
 
 describe("<NavigationItems />", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<NavigationItems />);
+  });
+
   it("should render two <NavigationItem /> elements if not authenticated", () => {
-    const wrapper = shallow(<NavigationItems />);
     // expect navigation items to render two navigation item components since the shallow copy won't authenticate
     expect(wrapper.find(NavigationItem)).toHaveLength(2);
+  });
+  it("should render three <NavigationItem /> elements if authenticated", () => {
+    wrapper.setProps({ isAuthenticated: true });
+    // expect navigation items to render three navigation item components
+    expect(wrapper.find(NavigationItem)).toHaveLength(3);
   });
 });
